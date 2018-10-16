7549a2551b68985c9a46d0d990c7ca8e9e15fb38
ithelost <0908048@hr.nl>
2018-10-16 12:20:58 +0200

    test hazard

---
+++ Assets/Scripts/GroundChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GroundChanger : MonoBehaviour {
+
+    public GameObject ground;
+    public Text text;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        //ground = GetComponentInChildren<Transform>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            text.text = "collsion active";
+            ground.SetActive(true);
+        }
+    }
+}
